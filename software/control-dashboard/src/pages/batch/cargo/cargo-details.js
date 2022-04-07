fa6b71d897da1ff8d177da4cfaf3e4d6fbdaca7b
renaultluk <ren66.rl@gmail.com>
2022-04-07 09:51:06 +0800

    7/4 Renault 2
    - fix cargo info page

--- software/control-dashboard/src/pages/batch/cargo/cargo-details.js
+++ software/control-dashboard/src/pages/batch/cargo/cargo-details.js
@@ -1,117 +1,54 @@
-import { Button, FormGroup, FormControlLabel, Switch, Typography, TextField } from '@mui/material';
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import { Button, FormGroup, FormControlLabel, Checkbox, Typography, TextField, FormLabel } from '@mui/material';
+import React, { useState } from 'react';
+import { withRouter, useHistory } from 'react-router-dom';
 
-class CargoInfo extends Component {
-    constructor(props) {
-        super(props);
-        this.state = props.cargo ? {
-            cargoID: props.cargo.cargoID,
-            requiresTemp: props.cargo.requiresTemp,
-            requiresHumidity: props.cargo.requiresHumidity,
-            tempLowerBound: props.cargo.tempLowerBound,
-            tempUpperBound: props.cargo.tempUpperBound,
-            humidityLowerBound: props.cargo.humidityLowerBound,
-            humidityUpperBound: props.cargo.humidityUpperBound,
-            isFragile: props.cargo.isFragile,
-            isUpright: props.cargo.isUpright,
-        } : {
-            cargoID: "",
-            requiresTemp: false,
-            requiresHumidity: false,
-            tempLowerBound: 0,
-            tempUpperBound: 0,
-            humidityLowerBound: 0,
-            humidityUpperBound: 0,
-            isFragile: false,
-            isUpright: false,
-        };
-    }
+const CargoInfo = () => {
+    const history = useHistory();
     
-    render() {
-        return (
-            <>
-                <Typography variant="h2">Enter Cargo Info</Typography>
-                <FormGroup>
-                    <TextField
-                        onChange={(e) => this.setState({ cargoID: e.target.value })}
-                        label="Cargo ID"
-                        variant="outlined"
-                        required
-                        defaultValue={this.props.cargo.cargoID}
-                    />
-                    <FormControlLabel 
-                        control={<Switch
-                            checked={this.state.requiresTemp}
-                            onChange={(e) => this.setState({ requiresTemp: e.target.checked })}
-                            />} 
-                        label="Has Temperature Requirements"
-                    />
-                    {this.state.requiresTemp ?
-                        <div>
-                            <TextField
-                                onChange={(e) => this.setState({ tempLowerBound: e.target.value })}
-                                label="Temperature Lower Bound"
-                                variant="outlined"
-                                type="number"
-                                defaultValue={this.props.cargo.tempLowerBound}
-                            />
-                            <TextField
-                                onChange={(e) => this.setState({ tempUpperBound: e.target.value })}
-                                label="Temperature Upper Bound"
-                                variant="outlined"
-                                type="number"
-                                defaultValue={this.props.cargo.tempUpperBound}
-                            />
-                        </div>
-                    : null }
-                    <FormControlLabel
-                        control={<Switch
-                            checked={this.state.requiresHumidity}
-                            onChange={(e) => this.setState({ requiresHumidity: e.target.checked })}
-                            />}
-                        label="Has Humidity Requirements"
-                    />
-                    {this.state.requiresHumidity ?
-                        <div>
-                            <TextField
-                                onChange={(e) => this.setState({ humidityLowerBound: e.target.value })}
-                                label="Humidity Lower Bound"
-                                variant="outlined"
-                                type="number"
-                                defaultValue={this.props.cargo.humidityLowerBound}
-                            />
-                            <TextField
-                                onChange={(e) => this.setState({ humidityUpperBound: e.target.value })}
-                                label="Humidity Upper Bound"
-                                variant="outlined"
-                                type="number"
-                                defaultValue={this.props.cargo.humidityUpperBound}
-                            />
-                        </div> 
-                    : null }
-                    <FormControlLabel
-                        control={<Switch
-                            checked={this.state.isFragile}
-                            onChange={(e) => this.setState({ isFragile: e.target.checked })}
-                            />}
-                        label="Is Fragile"
-                    />
-                    <FormControlLabel
-                        control={<Switch
-                            checked={this.state.isUpright}
-                            onChange={(e) => this.setState({ isUpright: e.target.checked })}
-                            />}
-                        label="Needs to be Upright"
-                    />
-
-                    <Button>
-                        Submit
-                    </Button>
-                </FormGroup>
-            </>
-        );
+    const defaultCargo = {
+        cargoID: "",
+        isFragile: false,
+        isUpright: false,
     }
+
+    const [cargo, setCargo] = useState(defaultCargo);
+    
+    return (
+        <>
+            <Typography variant="h2">Enter Cargo Info</Typography>
+            <FormGroup>
+                <TextField
+                    onChange={(e) => setCargo({ ...cargo, cargoID: e.target.value })}
+                    label="Cargo ID"
+                    variant="outlined"
+                    required
+                    defaultValue={cargo.cargoID}
+                />
+                <FormControlLabel
+                    control={<Checkbox
+                        checked={cargo.isFragile}
+                        onChange={(e) => setCargo({ ...cargo, isFragile: e.target.checked })}
+                        />}
+                    label="Is Fragile"
+                />
+                <FormControlLabel
+                    control={<Checkbox
+                        checked={cargo.isUpright}
+                        onChange={(e) => setCargo({ ...cargo, isUpright: e.target.checked })}
+                        />}
+                    label="Needs to be Upright"
+                />
+
+                <Button
+                    onClick={() => {
+                        history.goBack();
+                    }}
+                >
+                    Submit
+                </Button>
+            </FormGroup>
+        </>
+    );
 }
 
 export default withRouter(CargoInfo);
--- software/control-dashboard/src/pages/batch/cargo/index.js
+++ software/control-dashboard/src/pages/batch/cargo/index.js
@@ -1,19 +1,23 @@
-import { Button, Typography, Grid, TextField, Switch } from '@mui/material';
+import { Button, Typography, Grid, TextField, Checkbox, FormGroup, FormLabel, FormControlLabel } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
 import React, { useState } from 'react';
-import { withRouter, useHistory } from 'react-router-dom';
+import { withRouter, useHistory, useRouteMatch, Route, Switch } from 'react-router-dom';
 import { Container } from 'react-bootstrap';
 
 import { db } from '../../../config/my-firebase';
 import { ref, push, set } from 'firebase/database';
 
+import CargoInfo from './cargo-details';
+
 import useQuery from '../../../utils/useQuery';
 
-const BatchInfo = (props) => {
+const BatchInfo = () => {
     const query = useQuery();
     const batchID = query.get('batchID');
 
     const history = useHistory();
+
+    const { path, url } = useRouteMatch();
 
     const defaultBatch = {
         batchID: "",
@@ -41,157 +45,147 @@ const BatchInfo = (props) => {
     console.log("batch info");
 
         return (
-            <>
-                <Grid container spacing={3}>
-                    <Grid item xs={12}>
-                        <Typography variant="h2">Batch Info</Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Grid container spacing={3}>
-                            <Grid item xs={12}>
-                                <Typography>
-                                    {
-                                        batchID ?
-                                            `Batch ID: ${batch.batchID}` :
-                                            "New Batch"
-                                    }
-                                </Typography>
-                                
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Typography>Truck ID: </Typography>
-                                <TextField
-                                    label="Truck ID"
-                                    value={batch.truckID}
-                                    onChange={(e) => {
-                                        setBatch({
-                                            ...batch,
-                                            truckID: e.target.value
-                                        });
-                                    }}
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <Typography>Requires Temperature: </Typography>
-                                <Switch
-                                    checked={batch.requiresTemp}
-                                    onChange={(e) => {
-                                        setBatch({
-                                            ...batch,
-                                            requiresTemp: e.target.checked
-                                        });
-                                    }}
+            <Switch>
+                <Route exact path={path}>
+                    <Grid container spacing={3}>
+                        <Grid item xs={12}>
+                            <Typography variant="h2">Batch Info</Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FormGroup>
+                                    <FormLabel>
+                                        {
+                                            batchID ?
+                                                `Batch ID: ${batch.batchID}` :
+                                                "New Batch"
+                                        }
+                                    </FormLabel>
+                                    <FormControlLabel 
+                                        label="Truck ID"
+                                        control={
+                                            <TextField 
+                                                value={batch.truckID}
+                                                onChange={(e) => setBatch({ ...batch, truckID: e.target.value })}
+                                            />
+                                        }
                                     />
-                                    {batch.requiresTemp ? 
-                                        <Grid item xs={12}>
-                                            <Typography>Temperature Lower Bound: {batch.tempLowerBound}</Typography>
-                                            <TextField
-                                                label="Temperature Lower Bound"
-                                                value={batch.tempLowerBound}
-                                                onChange={(e) => {
-                                                    setBatch({
-                                                        ...batch,
-                                                        tempLowerBound: e.target.value
-                                                    });
-                                                }}
+                                    <FormControlLabel
+                                        label="Requires Temperature"
+                                        control={
+                                            <Checkbox
+                                                checked={batch.requiresTemp}
+                                                onChange={(e) => setBatch({ ...batch, requiresTemp: e.target.checked })}
                                             />
-                                            <Typography>Temperature Upper Bound: {batch.tempUpperBound}</Typography>
-                                            <TextField
-                                                label="Temperature Upper Bound"
-                                                value={batch.tempUpperBound}
-                                                onChange={(e) => {
-                                                    setBatch({
-                                                        ...batch,
-                                                        tempUpperBound: e.target.value
-                                                    });
-                                                }}
+                                        }
+                                        />
+                                        { batch.requiresTemp ?
+                                            <>
+                                                <FormControlLabel
+                                                    label="Temperature Lower Bound"
+                                                    control={
+                                                        <TextField
+                                                            value={batch.tempLowerBound}
+                                                            onChange={(e) => setBatch({ ...batch, tempLowerBound: e.target.value })}
+                                                        />
+                                                    }
+                                                /> 
+                                                <FormControlLabel
+                                                    label="Temperature Upper Bound"
+                                                    control={
+                                                        <TextField
+                                                            value={batch.tempUpperBound}
+                                                            onChange={(e) => setBatch({ ...batch, tempUpperBound: e.target.value })}
+                                                        />
+                                                    }
+                                                />
+                                            </> :
+                                            null
+                                        }
+                                    <FormControlLabel
+                                        label="Requires Humidity"
+                                        control={
+                                            <Checkbox
+                                                checked={batch.requiresHumidity}
+                                                onChange={(e) => setBatch({ ...batch, requiresHumidity: e.target.checked })}
                                             />
-                                        </Grid> :
+                                        }
+                                    />
+                                    { batch.requiresHumidity ?
+                                        <>
+                                            <FormControlLabel
+                                                label="Humidity Lower Bound"    
+                                                control={
+                                                    <TextField
+                                                        value={batch.humidityLowerBound}
+                                                        onChange={(e) => setBatch({ ...batch, humidityLowerBound: e.target.value })}
+                                                    />
+                                                }  
+                                            />
+                                            <FormControlLabel
+                                                label="Humidity Upper Bound"
+                                                control={
+                                                    <TextField
+                                                        value={batch.humidityUpperBound}
+                                                        onChange={(e) => setBatch({ ...batch, humidityUpperBound: e.target.value })}
+                                                    />
+                                                }
+                                            />
+                                        </> :
                                         null
-                                    }
-                                <Typography>Requires Humidity: </Typography>
-                                <Switch
-                                    checked={batch.requiresHumidity}
-                                    onChange={(e) => {
-                                        setBatch({
-                                            ...batch,
-                                            requiresHumidity: e.target.checked
-                                        });
-                                    }}
+                                }
+                            </FormGroup>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Button>
+                                Load CSV
+                            </Button>
+                            <Button onClick={() => history.push(`${url}/cargo`)} >
+                                Add Manually
+                            </Button>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <div style={{ height: '500px' }}>
+                                <DataGrid
+                                    columns={[
+                                        { field: 'cargo-id', headerName: 'Cargo ID' },
+                                        { field: 'fragile', headerName: 'Is Fragile' },
+                                        { field: 'is-upright', headerName: 'Needs to be Upright' },
+                                    ]}
+                                    rows={batch.cargo}
                                 />
-                            </Grid>
-                            {batch.requiresHumidity ?
-                                <Grid item xs={12}>
-                                    <Typography>Humidity Lower Bound:</Typography>
-                                    <TextField
-                                        label="Humidity Lower Bound"
-                                        value={batch.humidityLowerBound}
-                                        onChange={(e) => {
-                                            setBatch({
-                                                ...batch,
-                                                humidityLowerBound: e.target.value
-                                            });
-                                        }}
-                                    />
-                                    <Typography>Humidity Upper Bound:</Typography>
-                                    <TextField
-                                        label="Humidity Upper Bound"
-                                        value={batch.humidityUpperBound}
-                                        onChange={(e) => {
-                                            setBatch({
-                                                ...batch,
-                                                humidityUpperBound: e.target.value
-                                            });
-                                        }}
-                                    />
-                                </Grid> :
-                                null
-                            }
+                            </div>
+                        </Grid>
+
+                        <Grid item xs={12}>
+                            <Button
+                                onClick={
+                                    () => {
+                                        setBatch(defaultBatch);
+                                        history.push("/main");
+                                    }
+                                }
+                            >
+                                Cancel
+                            </Button>
+                            <Button>
+                                Save
+                            </Button>
+                            <Button
+                                onClick={() => history.push("/main")}
+                            >
+                                Post
+                            </Button>
                         </Grid>
                     </Grid>
-
-                    <Grid item xs={12}>
-                        <Button>
-                            Load CSV
-                        </Button>
-                        <Button onClick={() => history.push("/cargo-info")} >
-                            Add Manually
-                        </Button>
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <DataGrid
-                            columns={[
-                                { field: 'cargo-id', headerName: 'Cargo ID' },
-                                { field: 'fragile', headerName: 'Is Fragile' }
-                            ]}
-                            rows={batch.cargo}
-                        />
-                    </Grid>
-
-                    <Grid item xs={12}>
-                        <Button
-                            onClick={
-                                () => {
-                                    setBatch(defaultBatch);
-                                    history.push("/main");
-                                }
-                            }
-                        >
-                            Cancel
-                        </Button>
-                        <Button>
-                            Save
-                        </Button>
-                        <Button>
-                            Post
-                        </Button>
-                    </Grid>
-                </Grid>
-
-            </>
+                </Route>
+                <Route path={`${path}/cargo`}>
+                    <CargoInfo />
+                </Route>
+            </Switch>
         );
     // }
 }
 
-export default BatchInfo;
+export default withRouter(BatchInfo);
